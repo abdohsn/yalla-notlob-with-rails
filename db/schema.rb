36d9f954120f1e2fd3402cd29ab1e2f15397c936
Abdelrahaman-Hassan <abdohsn39@yahoo.com>
2021-04-26 01:20:15 +0200

    add image

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_053004) do
+ActiveRecord::Schema.define(version: 2021_04_25_203524) do
 
-ActiveRecord::Schema.define(version: 2021_04_19_065930) do
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
 
-  create_table "friendships", charset: "utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "friendships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "group_members", charset: "utf8", force: :cascade do |t|
+  create_table "group_members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "group_id"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
@@ -30,7 +56,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_065930) do
     t.index ["user_id"], name: "index_group_members_on_user_id"
   end
 
-  create_table "groups", charset: "utf8", force: :cascade do |t|
+  create_table "groups", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
@@ -38,8 +64,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_065930) do
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
-
-  create_table "order_details", charset: "utf8", force: :cascade do |t|
+  create_table "order_details", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "item_name"
     t.integer "amount"
     t.float "price"
@@ -50,7 +75,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_065930) do
     t.index ["user_order_join_id"], name: "index_order_details_on_user_order_join_id"
   end
 
-  create_table "orders", charset: "utf8", force: :cascade do |t|
+  create_table "orders", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "orderType"
     t.string "orderFrom"
     t.string "menuImage"
@@ -61,7 +86,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_065930) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "sessions", charset: "utf8", force: :cascade do |t|
+  create_table "sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "session_id", null: false
     t.text "data"
     t.datetime "created_at", precision: 6, null: false
@@ -70,7 +95,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_065930) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "user_order_joins", charset: "utf8", force: :cascade do |t|
+  create_table "user_order_joins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "order_id"
     t.datetime "created_at", precision: 6, null: false
@@ -79,7 +104,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_065930) do
     t.index ["user_id"], name: "index_user_order_joins_on_user_id"
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "full_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -108,6 +133,8 @@ ActiveRecord::Schema.define(version: 2021_04_19_065930) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "group_members", "groups"
   add_foreign_key "group_members", "users"
   add_foreign_key "groups", "users"
